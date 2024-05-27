ca691e0e8e6e90565e066a88f954a15747a34712
SupLeonardo <levabaluevvv@gmail.com>
2024-05-27 15:40:11 +0500

    ppp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,18 +21,29 @@ let array: {
 
 
 onMount(() => {
-    async function Modal(display: string) {
-        modal.style.display = display
+    async function Modal(opacity: string) {
+        modal.style.opacity = opacity
     }
     let modal = document.querySelector('#modal') as HTMLDivElement
-    let button = document.getElementById('button')
+    let button = document.querySelector('#button')
+    let button2 = document.querySelector('#button2') as HTMLDivElement
     button.addEventListener('click', () => {
-        const modalDisplayStyle = getComputedStyle(modal).display;
-        if (modalDisplayStyle === 'none') {
-            Modal('flex')
+        const modalDisplayStyle = getComputedStyle(modal).opacity;
+        if (modalDisplayStyle === '0') {
+            Modal('1')
+            button2.style.opacity = '1'
+            button.innerHTML = "Submit"
         } else {
-            Modal('none')
+            Modal('0')
+            button2.style.opacity = '0'
+            button.innerHTML = "Add item"
         }
+    })
+    button2.addEventListener('click', () => {
+        const modalDisplayStyle = getComputedStyle(modal).opacity;
+        Modal('0')
+            button2.style.opacity = '0'
+            button.innerHTML = "Add item"
     })
 });
 
@@ -53,13 +64,14 @@ onMount(() => {
         {/each}
     </ul>
     </div>
-    <div id='modal' class="fixed hidden left-0 justify-start py-[2vh] px-[5vw] flex-col top-[calc(50%-16vh)] right-0 z-20 bg-second mx-[2vw] rounded-lg h-[36vh] ">
-        <h1 class="text-white text-3xl font-bold flex justify-center">Enter a task</h1>
+    <div id='modal' class="fixed flex opacity-0 transition-all duration-500 ease-in-out left-0 justify-start py-[2vh] px-[5vw] flex-col top-[calc(50%-16vh)] right-0 z-20 bg-second mx-[2vw] rounded-lg h-[36vh] ">
+        <h1 class="text-white text-3xl font-bold flex justify-center">Enter a name</h1>
         <input class="text-white text-xl bg-black mt-[2vh] border-4 border-spotify focus-visible:outline-none rounded-xl px-[1vh]">
-        <h1 class="text-white text-3xl flex justify-center mt-[2vh] font-bold">Enter a text</h1>
+        <h1 class="text-white text-3xl flex justify-center mt-[2vh] font-bold">Enter a description</h1>
         <textarea class="bg-black text-white text-start text-xl focus-visible:outline-none border-4 border-spotify p-3 rounded-xl h-full mt-[2vh]"></textarea>
     </div>
-    <div class="bg-black fixed bottom-0 left-0 right-0 z-10 h-[6vh] rounded-lg text-black font-bold text-2xl">
+    <div class="bg-black fixed bottom-0 left-0 right-0 z-10 transition-all duration-700 ease-in-out h-[6vh] rounded-lg text-black font-bold text-2xl">
+        <div id="button2" class="flex justify-center opacity-0 items-center bg-black border-4 border-spotify cursor-pointer fixed bottom-0 left-0 right-0 mx-[2vw] z-10 mb-[13vh] h-[6vh] rounded-lg text-spotify font-bold text-2xl">Cancel</div>
         <div id='button' class="flexix bg-spotify cursor-pointer fixed bottom-0 left-0 right-0 mx-[2vw] z-10 mb-[6vh] h-[6vh] rounded-lg text-black font-bold text-2xl">Add item</div>
     </div>
 </div>
