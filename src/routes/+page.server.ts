fd142fa2c46fa402a0143f73ab7bc4120c562426
SupLeonardo <levabaluevvv@gmail.com>
2024-06-11 20:46:11 +0500

    asa

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,30 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import type { Actions } from './$types';
+import type { PageServerLoad } from './$types';
+import { goto } from '$app/navigation';
 
 const prisma = new PrismaClient()
-export const actions = {	
-    sign_in: async ({request}) => {
-        const {nickname, password} = Object.fromEntries(await request.formData()) as {
-            nickname: string;
-            password: string;
-        }
-        const users = await prisma.user.findMany({
-            select: {
-                name: true,
-                password: true
-            }
-        })
-        let names: string[];
-        let passwords: string[];
-        names = users.map(item => item.name)
-        passwords = users.map(item => item.password)
-        if (names.includes(nickname) && passwords.includes(password)) {
-            // succesfully logged in
-        } else if (!(names.includes(nickname)) && passwords.includes(password)) {
-            alert('Nickname already in use')
-        } else if (!(names.includes(nickname)) && !(passwords.includes(password))) {
-            alert('Incorrect password or nickname')
-        }
-        // console.log('Hi')
+export const actions = {
+    default: async ({ request }) => {
+        console.log(request.body)
     }
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,24 +15,38 @@
     import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
     import {browser} from '$app/environment';
+    import type { PageData } from './$types';
 
-    async function toMain() {
-        await goto('/main-page')
+    export let data: PageData;
+    console.log(data)
+
+    async function signIn() {
+        const data = { a, b };
+        const jsonString = JSON.stringify(data);
+
+        const response = await fetch('src/routes/+page.server.ts', {
+            method: 'POST',
+            body: jsonString,
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        const result = await response.json();
+        console.log(result);
     }
 
-    export let nickname: string;
-    export let password: string;
+    let a: string;
+    let b: string;
 
-    onMount(() => {
-        let nick = document.querySelector('#nickname') as HTMLInputElement
-        let pass = document.querySelector('#password') as HTMLInputElement
-        nick.addEventListener('input', () => {
-            nickname = nick.value
-        })
-        pass.addEventListener('input', () => {
-            password = pass.value
-        })
-    })
+    // onMount(() => {
+    //     let nick = document.querySelector('#nickname') as HTMLInputElement
+    //     let pass = document.querySelector('#password') as HTMLInputElement
+    //     nick.addEventListener('input', () => {
+    //         nickname = nick.value
+    //     })
+    //     pass.addEventListener('input', () => {
+    //         password = pass.value
+    //     })
+    // })
 </script>
 
 <div class="flexix flex-col h-full px-[2vw] pb-[1.5vh] pt-[10vh] ">
@@ -40,13 +54,13 @@
         <h1 class='font-bold text-5xl'>Welcome!</h1>
         <div class="bg-second rounded-lg flex flex-col items-center px-[5vw] w-full mt-[5vh] h-[45vh] ">  
             <h2 class="font-bold mt-[5vh] ">Nickname</h2>
-            <input id="nickname" name="nickname" class="text-white text-xl w-full bg-black mt-[2vh] border-4 border-spotify focus-visible:outline-none rounded-xl px-[1vh]">
+            <input id="nickname" bind:value={a} class="text-white text-xl w-full bg-black mt-[2vh] border-4 border-spotify focus-visible:outline-none rounded-xl px-[1vh]">
             <h2 class="font-bold mt-[4vh] ">Password</h2>
-            <input id="password" name="password" class="text-white w-full text-xl bg-black mt-[2vh] border-4 border-spotify focus-visible:outline-none rounded-xl px-[1vh]">
+            <input id="password" bind:value={b} class="text-white w-full text-xl bg-black mt-[2vh] border-4 border-spotify focus-visible:outline-none rounded-xl px-[1vh]">
         </div>
         <div>
-            <button id='button' formaction="?/create" class="flexix bg-spotify cursor-pointer fixed bottom-0 left-0 right-0 mx-[2vw] z-10 mb-[4vh] h-[6vh] rounded-lg text-black font-bold text-2xl">Create account</button>
-            <button id='button' formaction="?/sign_in" class="flexix bg-second cursor-pointer fixed bottom-0 left-0 right-0 mx-[2vw] z-10 mb-[11vh] h-[6vh] rounded-lg text-spotify font-bold text-2xl">Sign in</button>
+            <button id='button'  class="flexix bg-spotify cursor-pointer fixed bottom-0 left-0 right-0 mx-[2vw] z-10 mb-[4vh] h-[6vh] rounded-lg text-black font-bold text-2xl">Create account</button>
+            <button id='button' on:click={signIn} class="flexix bg-second cursor-pointer fixed bottom-0 left-0 right-0 mx-[2vw] z-10 mb-[11vh] h-[6vh] rounded-lg text-spotify font-bold text-2xl">Sign in</button>
         </div>
     </form>
     
